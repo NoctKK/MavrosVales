a444db20b1e347779602f0d343e0768af2a8a22a
NoctKK <106299991+NoctKK@users.noreply.github.com>
2026-03-15 00:45:23 +0200

    Update constants.js

--- game/constants.js
+++ game/constants.js
@@ -1,1 +1,24 @@
+const TURN_TIME_MS = 60000;
+const LOBBY_IDLE_MS = 120000;
+const ROUND_RESTART_MS = 4000;
+const DEAL_INTERVAL_MS = 50;
+const STARTING_HAND_SIZE = 11;
+const MAX_SCORE = 500;
+const MAX_NAME_LEN = 15;
+const MAX_CHAT_LEN = 80;
 
+const SUITS = ['♠', '♣', '♥', '♦'];
+const VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+module.exports = {
+    TURN_TIME_MS,
+    LOBBY_IDLE_MS,
+    ROUND_RESTART_MS,
+    DEAL_INTERVAL_MS,
+    STARTING_HAND_SIZE,
+    MAX_SCORE,
+    MAX_NAME_LEN,
+    MAX_CHAT_LEN,
+    SUITS,
+    VALUES
+};
