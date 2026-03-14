50026f4d096d24dadc8584e05e4e68a5296a34b7
NoctKK <106299991+NoctKK@users.noreply.github.com>
2026-03-15 01:22:54 +0200

    Update registerSocketHandlers.js

--- sockets/registerSocketHandlers.js
+++ sockets/registerSocketHandlers.js
@@ -1,55 +1,54 @@
-function registerSocketHandlers(io, globalGameInstance) {
-    io.on('connection', (socket) => {
-        if (!globalGameInstance.gameStarted) {
-            globalGameInstance.refreshLobbyTimer();
-        }
+module.exports = function registerSocketHandlers(io, socket, game) {
+    if (!game.gameStarted) {
+        game.refreshLobbyTimer();
+    }
 
-        socket.on('joinGame', (data) => {
-            globalGameInstance.joinGame(socket, data);
-        });
-
-        socket.on('startGameRequest', () => {
-            globalGameInstance.refreshLobbyTimer();
-
-            const activeCount = globalGameInstance.playerOrder.filter(
-                id => globalGameInstance.players[id] && globalGameInstance.players[id].connected
-            ).length;
-
-            if (!globalGameInstance.gameStarted && !globalGameInstance.starting && activeCount >= 2) {
-                globalGameInstance.starting = true;
-                globalGameInstance.startNewRound(true);
-            }
-        });
-
-        socket.on('playCard', (data) => {
-            globalGameInstance.playCard(socket, data);
-        });
-
-        socket.on('drawCard', () => {
-            globalGameInstance.drawCard(socket);
-        });
-
-        socket.on('passTurn', () => {
-            globalGameInstance.passTurn(socket);
-        });
-
-        socket.on('chatMessage', (msg) => {
-            globalGameInstance.refreshLobbyTimer();
-            const p = globalGameInstance.players[socket.id];
-
-            if (p && (!p.lastChat || Date.now() - p.lastChat > 500)) {
-                p.lastChat = Date.now();
-                io.emit('chatUpdate', {
-                    name: p.name,
-                    text: String(msg).replace(/[<>]/g, '').substring(0, 80)
-                });
-            }
-        });
-
-        socket.on('disconnect', () => {
-            globalGameInstance.disconnectPlayer(socket.id);
-        });
+    socket.on('joinGame', (data) => {
+        game.joinGame(socket, data);
     });
-}
 
-module.exports = registerSocketHandlers;
+    socket.on('startGameRequest', () => {
+        game.refreshLobbyTimer();
+
+        const activeCount = game.playerOrder.filter(
+            id => game.players[id] && game.players[id].connected
+        ).length;
+
+        if (!game.gameStarted && !game.starting && activeCount >= 2) {
+            game.starting = true;
+            game.startNewRound(true);
+        }
+    });
+
+    socket.on('playCard', (data) => {
+        game.playCard(socket, data);
+    });
+
+    socket.on('drawCard', () => {
+        game.drawCard(socket);
+    });
+
+    socket.on('passTurn', () => {
+        game.passTurn(socket);
+    });
+
+    socket.on('chatMessage', (msg) => {
+        game.refreshLobbyTimer();
+
+        const p = game.players[socket.id];
+        if (!p) return;
+
+        if (!p.lastChat || Date.now() - p.lastChat > 500) {
+            p.lastChat = Date.now();
+
+            io.emit('chatUpdate', {
+                name: p.name,
+                text: String(msg).replace(/[<>]/g, '').substring(0, 80)
+            });
+        }
+    });
+
+    socket.on('disconnect', () => {
+        game.disconnectPlayer(socket.id);
+    });
+};
