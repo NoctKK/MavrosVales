2c92d7c9ec681c271753ed04c5c5d27edb53d8c0
NoctKK <106299991+NoctKK@users.noreply.github.com>
2026-03-15 00:36:27 +0200

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,1 +1,16 @@
+const socket = io();
 
+let myId = null;
+let lastClick = 0;
+const CLICK_DELAY = 220;
+let actionLocked = false;
+let gameScale = 1;
+let selectedAceIndex = null;
+let lastPlayerId = null;
+let timerEnd = 0;
+let animationFrameId = null;
+let fullScoreHistory = [];
+let isScoreboardExpanded = false;
+let msgQueue = [];
+let isMsgShowing = false;
+let lastDiscardCount = 0;
