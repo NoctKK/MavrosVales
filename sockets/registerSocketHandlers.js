4a948bf96e57a6646daccca8a193ac0d8f664d9e
NoctKK <106299991+NoctKK@users.noreply.github.com>
2026-03-15 00:47:10 +0200

    Update registerSocketHandlers.js

--- sockets/registerSocketHandlers.js
+++ sockets/registerSocketHandlers.js
@@ -1,1 +1,55 @@
+const { MAX_CHAT_LEN } = require('../game/constants');
 
+function registerSocketHandlers(io, game) {
+    io.on('connection', (socket) => {
+        if (!game.gameStarted) game.refreshLobbyTimer();
+
+        socket.on('joinGame', (data) => {
+            game.joinGame(socket, data);
+        });
+
+        socket.on('startGameRequest', () => {
+            game.refreshLobbyTimer();
+
+            const activeCount = game.playerOrder.filter(
+                id => game.players[id] && game.players[id].connected
+            ).length;
+
+            if (!game.gameStarted && !game.starting && activeCount >= 2) {
+                game.starting = true;
+                game.startNewRound(true);
+            }
+        });
+
+        socket.on('playCard', (data) => {
+            game.playCard(socket, data);
+        });
+
+        socket.on('drawCard', () => {
+            game.drawCard(socket);
+        });
+
+        socket.on('passTurn', () => {
+            game.passTurn(socket);
+        });
+
+        socket.on('chatMessage', (msg) => {
+            game.refreshLobbyTimer();
+            const p = game.players[socket.id];
+
+            if (p && (!p.lastChat || Date.now() - p.lastChat > 500)) {
+                p.lastChat = Date.now();
+                io.emit('chatUpdate', {
+                    name: p.name,
+                    text: String(msg).replace(/[<>]/g, '').substring(0, MAX_CHAT_LEN)
+                });
+            }
+        });
+
+        socket.on('disconnect', () => {
+            game.disconnectPlayer(socket.id);
+        });
+    });
+}
+
+module.exports = registerSocketHandlers;
